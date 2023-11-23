9dbcc993078b0b3bddd44dadf4b0402fea3684ee
Muhammad Kaif <xcoder304@gmail.com>
2023-11-24 00:14:39 +0500

    fix(')

--- src/modules/posts/dto/create-post.dto.ts
+++ src/modules/posts/dto/create-post.dto.ts
@@ -1,60 +1,57 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 import { PostType } from 'src/entities/posts.entity';
 import { User } from 'src/entities/users.entity';
 
 export class CreatePostDto {
   @Expose()
   @ApiProperty()
-  id: string;
-
-  @Expose()
-  @ApiProperty()
+  @IsNotEmpty()
   title: string;
 
   @Expose()
   @ApiProperty()
+  @IsNotEmpty()
   body: string;
 
   @Expose()
   @ApiProperty()
+  @IsOptional()
   media: string[];
 
   @Expose()
   @ApiProperty()
+  @IsOptional()
   tags: string[];
 
   @Expose()
   @ApiProperty()
+  @IsOptional()
   info: Record<string, any> | null;
 
   @Expose()
   @ApiProperty()
+  @IsOptional()
   likes: number;
 
   @Expose()
   @ApiProperty()
+  @IsOptional()
   postType: PostType;
 
   @Expose()
   @ApiProperty()
+  @IsOptional()
   location: string;
 
   @Expose()
   @ApiProperty()
-  @Exclude() // Exclude from the response, as it's not needed when creating or updating
+  @IsNotEmpty()
   postedBy: User;
 
   @Expose()
   @ApiProperty()
+  @IsOptional()
   comments: Comment[];
-
-  @Expose()
-  createdAt: Date;
-
-  @Expose()
-  updatedAt: Date;
-
-  @Expose()
-  deletedAt: Date;
 }
